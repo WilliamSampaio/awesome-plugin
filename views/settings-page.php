2a7a71e49831349728807b5e886bba5ff37049a0
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-09-01 15:40:12 -0400

    renderiza as options na view

--- views/settings-page.php
+++ views/settings-page.php
@@ -1,6 +1,10 @@
 <div class="wrap">
     <h1><?= esc_html(get_admin_page_title()) ?></h1>
     <form action="options.php" method="post">
-        
+        <?php
+        settings_fields('ap_slider_group');
+        do_settings_sections('ap_slider_page1');
+        submit_button('Save Settings');
+        ?>
     </form>
 </div>
