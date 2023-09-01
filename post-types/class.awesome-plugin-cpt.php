d35a35dcd7032c87d28ccecfbf4b6dda210c9d1b
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-09-01 00:53:03 -0400

    meta boxes

--- post-types/class.awesome-plugin-cpt.php
+++ post-types/class.awesome-plugin-cpt.php
@@ -8,6 +8,10 @@ if (!class_exists('AwesomePlugin_Post_Type')) {
         function __construct()
         {
             add_action('init', [$this, 'create_post_type']);
+            add_action('add_meta_boxes', [
+                $this, 'add_meta_boxes'
+            ]);
+            add_action('save_post', [$this, 'save_post'], accepted_args: 2);
         }
 
         public function create_post_type()
@@ -35,9 +39,40 @@ if (!class_exists('AwesomePlugin_Post_Type')) {
                 'has_archive' => false,
                 'exclude_from_search' => false,
                 'publicly_queryable' => true,
-                'show_in_rest' => true,
-                'menu_icon' => 'dashicons-images-alt2'
+                'show_in_rest' => false,
+                'menu_icon' => 'dashicons-images-alt2',
+                //'register_meta_box_cb' => [$this, 'add_meta_boxes']
             ]);
+        }
+
+        public function add_meta_boxes()
+        {
+            add_meta_box(
+                'ap_slider_meta_box',
+                'Link Options',
+                [$this, 'add_inner_meta_boxes'],
+                'ap-slider',
+                'normal',
+                'high'
+            );
+        }
+
+        public function add_inner_meta_boxes($post)
+        {
+            require_once(AWESOME_PLUGIN_PATH . 'views/ap-slider_metabox.php');
+        }
+
+        public function save_post($post_id)
+        {
+            if (isset($_POST['action']) && $_POST['action'] == 'editpost') {
+                $old_link_text = get_post_meta($post_id, 'ap_slider_link_text');
+                $new_link_text = $_POST['ap_slider_link_text'];
+                $old_link_url = get_post_meta($post_id, 'ap_slider_link_url');
+                $new_link_url = $_POST['ap_slider_link_url'];
+
+                update_post_meta($post_id, 'ap_slider_link_text', $new_link_text, $old_link_text);
+                update_post_meta($post_id, 'ap_slider_link_url', $new_link_url, $old_link_url);
+            }
         }
     }
 }
---
+++ views/ap-slider_metabox.php
@@ -0,0 +1,18 @@
+<table class="form-table ap-slider-metabox">
+    <tr>
+        <th>
+            <label for="ap_slider_link_text">Link Text</label>
+        </th>
+        <td>
+            <input type="text" name="ap_slider_link_text" id="ap_slider_link_text" class="regular-text link-text" value="" required>
+        </td>
+    </tr>
+    <tr>
+        <th>
+            <label for="ap_slider_link_url">Link Url</label>
+        </th>
+        <td>
+            <input type="url" name="ap_slider_link_url" id="ap_slider_link_url" class="regular-text link-url" value="" required>
+        </td>
+    </tr>
+</table>
